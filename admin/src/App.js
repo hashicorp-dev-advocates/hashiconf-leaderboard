62f55fdb457bce8e265ab5fda5d08a75dc15f19d
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-08-19 16:58:48 -0400

    Make sure delete ability is on another page

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EscapeRoom from "./pages/EscapeRoom";
 import Robots from "./pages/Robots";
 import Home from "./pages/Home";
+import Delete from "./pages/Delete";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="escape-room" element={<EscapeRoom />} />
         <Route path="robots" element={<Robots />} />
+        <Route path="delete" element={<Delete />} />
       </Routes>
     </BrowserRouter>
   );
--- admin/src/pages/Home.js
+++ admin/src/pages/Home.js
@@ -1,60 +1,10 @@
-import { getTeams, deleteTeam } from "./api/leaderboard";
-import { useState, useEffect } from 'react';
-import DataTable from 'react-data-table-component';
-
-const columns = [
-	{
-		name: 'ID',
-		selector: row => row.id,
-    sortable: true,
-	},
-	{
-		name: 'Name',
-		selector: row => row.name,
-    sortable: true,
-	},
-	{
-		name: 'Activation',
-		selector: row => row.activation,
-    sortable: true,
-	},
-	{
-		name: 'Time',
-		selector: row => row.time,
-	},
-	{
-		name: 'Delete',
-    cell: row => (
-      <button type="delete" onClick={() => deleteRow(row.id)}>delete</button>
-    )
-	},
-];
-
-async function deleteRow(id) {
-  let success = deleteTeam(id);
-  if (!success) {
-    alert("Did not delete team with id " + id);
-  }
-  window.location.reload(true);
-}
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-  const [teams, setTeams] = useState([]);
-
-  const getData = async () => {
-    let data = await getTeams();
-    setTeams(data);
-  }
-
-  useEffect(() => {
-    getData();
-   }, []);
-
   return (
     <>
       <h1>HashiConf Activations Admin Dashboard</h1>
-      <DataTable theme="dark" fixedHeader={true} columns={columns} data={teams}></DataTable>
+      <body><div class="container"><button><Link to="/delete">Need to delete a team?</Link></button></div></body>
     </>
-
   )
 }
