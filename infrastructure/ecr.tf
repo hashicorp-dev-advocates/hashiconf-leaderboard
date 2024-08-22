53ce63bb036df81013acdadfa15328a6a2e6e44f
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-08-22 13:52:49 -0400

    Add account ID to GH actions

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_ecr_repository" "service" {
   for_each     = var.leaderboard_services
   name         = "leaderboard-${each.value}"
@@ -13,4 +15,10 @@ resource "github_actions_variable" "service" {
   repository    = var.github_repository
   variable_name = each.value
   value         = aws_ecr_repository.service[each.value].repository_url
+}
+
+resource "github_actions_variable" "aws_account_id" {
+  repository    = var.github_repository
+  variable_name = "AWS_ACCOUNT_ID"
+  value         = data.aws_caller_identity.current.account_id
 }
