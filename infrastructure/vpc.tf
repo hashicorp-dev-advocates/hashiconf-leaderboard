8213e2c066802a076e756c6b2e1296c516627513
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-08-22 11:17:44 -0400

    Add back private subnets

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -15,10 +15,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.13.0"
 
-  name           = var.name
-  cidr           = var.vpc_cidr_block
-  azs            = data.aws_availability_zones.available.names
-  public_subnets = slice(local.subnets, 3, 6)
+  name            = var.name
+  cidr            = var.vpc_cidr_block
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = slice(local.subnets, 0, 3)
+  public_subnets  = slice(local.subnets, 3, 6)
 
   create_database_subnet_group       = true
   create_database_subnet_route_table = true
