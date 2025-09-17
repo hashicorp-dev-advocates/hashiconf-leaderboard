14ec38f008142ebc39bee565415a8ace7b7480ca
Michael Kosir <michael.kosir@hashicorp.com>
2025-09-17 17:02:39 -0500

    added sleep

--- test.sh
+++ test.sh
@@ -7,17 +7,20 @@ max=2000
 
 for i in {1..25}
 do
-    curl -s -XPOST -H "Authorization:$TOKEN" -d '{"name":"'$(curl -s https://randomuser.me/api/ | jq -r '.results[0].name.first')'","time":'$(($RANDOM%($max-$min+1)+$min))', "activation":"robots"}' localhost:9090/teams
+    curl -s -XPOST -H "Authorization:$TOKEN" -d '{"name":"'$(curl -s https://randomuser.me/api/ | jq -r '.results[0].name.first')'","time":'$(($RANDOM%($max-$min+1)+$min))', "activation":"robots"}' localhost:9090/teams;
+    sleep 0.1;
 done
 
 for i in {1..25}
 do
-    curl -s -XPOST -H "Authorization:$TOKEN" -d '{"name":"'$(curl -s https://randomuser.me/api/ | jq -r '.results[0].name.first')'","time":'$(($RANDOM%($max-$min+1)+$min))', "activation":"vpm"}' localhost:9090/teams
+    curl -s -XPOST -H "Authorization:$TOKEN" -d '{"name":"'$(curl -s https://randomuser.me/api/ | jq -r '.results[0].name.first')'","time":'$(($RANDOM%($max-$min+1)+$min))', "activation":"vpm"}' localhost:9090/teams;
+    sleep 0.1;
 done
 
 for i in {1..25}
 do
-    curl -s -XPOST -H "Authorization:$TOKEN" -d '{"name":"'$(curl -s https://randomuser.me/api/ | jq -r '.results[0].name.first')'","time":'$(($RANDOM%($max-$min+1)+$min))', "activation":"ai"}' localhost:9090/teams
+    curl -s -XPOST -H "Authorization:$TOKEN" -d '{"name":"'$(curl -s https://randomuser.me/api/ | jq -r '.results[0].name.first')'","time":'$(($RANDOM%($max-$min+1)+$min))', "activation":"ai"}' localhost:9090/teams;
+    sleep 0.1;
 done
 
 curl -s -H "Authorization:$TOKEN" localhost:9090/logout
